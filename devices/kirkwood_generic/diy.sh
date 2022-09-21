abe0a7a4bf03735edb24752375c59c0b2abdf656
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-09-21 16:36:03 +0800

    update

--- devices/kirkwood_generic/diy.sh
+++ devices/kirkwood_generic/diy.sh
@@ -5,3 +5,5 @@ shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 #bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
+curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/aa3f8bc7380f4c8526a7c70b67bbb7d820508891/target/linux/generic/hack-5.10/953-net-patch-linux-kernel-to-support-shortcut-fe.patch -o target/linux/generic/hack-5.10/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+
